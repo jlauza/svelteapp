4a699776d8a3bf110dc9f45aca2e4d9f9d4ce24d
jlauza <lauzajayson@gmail.com>
2024-10-19 22:24:39 +0800

    function testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script>
+	import Header from '../components/Header.svelte';
+
+	function test() {
+		return 'Hey';
+	}
 </script>
 
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Header />
+<h1>Welcome to Homepage.</h1>
+{test()}
